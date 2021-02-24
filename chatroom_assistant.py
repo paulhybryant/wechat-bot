ce8e5cd467716b9303479d362776430c61d07db3
paulhybryant <paulhybryant@gmail.com>
2021-02-24 14:03:03 +0800

    .

---
+++ chatroom_assistant.py
@@ -0,0 +1,107 @@
+# -*- coding=utf-8 -*-
+"""
+"""
+import os
+import asyncio
+import logging
+
+from wechaty import (
+    Contact,
+    FileBox,
+    Message,
+    MessageType,
+    Wechaty,
+)
+
+logging.basicConfig(level=logging.ERROR)
+log = logging.getLogger(__name__)
+bot = None
+counter = 0
+
+
+async def on_message(msg: Message):
+    """
+    Message Handler for the Bot
+    """
+    global counter
+    counter += 1
+    log.error('received %s messages' % counter)
+    me = bot.Contact.load('paulhybryant0104')
+    text = msg.text()
+    mention_self = await msg.mention_self()
+    mention_text = await msg.mention_text()
+    if msg.room():
+        topic = await msg.room().topic()
+        log.error('room: %s, topic %s' % (msg.room().room_id, topic))
+        # @me
+        if mention_self:
+            log.error('mentioned me')
+            await me.say('来自群: %s' % topic)
+            await msg.forward(me)
+        # @all
+        elif '@所有人' in mention_text or '@all' An mention_text:
+            log.error('mentioned all')
+            await me.say('来自群: %s' % topic)
+            await msg.forward(me)
+        # match string
+        elif '额度' in text:
+            log.error('contains 额度: %s' % text)
+            await me.say('来自群: %s' % topic)
+            await msg.forward(me)
+        elif topic == 'MyAssistant':
+            log.error(msg)
+            if msg.type() == MessageType.MESSAGE_TYPE_ATTACHMENT:
+                filebox = await msg.to_file_box()
+                await me.say(filebox)
+        elif msg.room().room_id == '4932234304@chatroom':
+            if msg.type() == MessageType.MESSAGE_TYPE_ATTACHMENT:
+                filebox = await msg.to_file_box()
+                await me.say(filebox)
+    else:
+        log.error(msg)
+        if text == '#你最喜欢谁':
+            await msg.say('当然是bibi了')
+        elif text == '#谁最可爱':
+            await msg.say('萌萌')
+
+
+async def on_scan(qrcode: str, status: int, data):
+    """
+    Scan Handler for the Bot
+    """
+    print('Status: ' + status + ', View QR Code Online: https://wechaty.github.io/qrcode/' + str(qrcode))
+
+
+async def on_login(user: Contact):
+    """
+    Login Handler for the Bot
+    """
+    print(user)
+
+async def main():
+    """
+    Async Main Entry
+    """
+    #
+    # Make sure we have set WECHATY_PUPPET_SERVICE_TOKEN in the environment variables.
+    #
+    if 'WECHATY_PUPPET_SERVICE_TOKEN' not in os.environ:
+        print('''
+            Error: WECHATY_PUPPET_SERVICE_TOKEN is not found in the environment variables
+            You need a TOKEN to run the Java Wechaty. Please goto our README for details
+            https://github.com/wechaty/python-wechaty-getting-started/#wechaty_puppet_service_token
+        ''')
+
+    global bot
+    bot = Wechaty()
+
+    bot.on('scan',      on_scan)
+    bot.on('login',     on_login)
+    bot.on('message',   on_message)
+
+    await bot.start()
+
+    print('[Python Wechaty] My Assistant Bot started.')
+
+
+asyncio.run(main())
